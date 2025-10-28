9a82613adf3ea1cef0ddd4d95e037761135b52c2
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-10-28 17:17:01 +0700

    fix: tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-      "./src/**/*.{js,ts,jsx,tsx}",
-     "./public/index.html",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./storage/framework/views/*.php",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.')[1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'images';
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+            },
+        },
+    },
 });
