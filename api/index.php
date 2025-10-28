c70eb338d75bc495edd7312bc294fa14ff07ebfa
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-10-28 16:31:08 +0700

    fix: change autolod in public/index.php and api/index.php use

--- api/index.php
+++ api/index.php
@@ -1,3 +1,12 @@
 <?php
 
+// Definisikan Document Root sebagai parent directory dari 'api' (Root Proyek Laravel)
+$_SERVER['DOCUMENT_ROOT'] = dirname(__DIR__);
+
+// Tambahkan ENV Laravel_ENV jika diperlukan (Vercel PHP sering membutuhkannya)
+$_SERVER['APP_ENV'] = 'production';
+$_SERVER['LARAVEL_ENV'] = 'production';
+$_SERVER['APP_URL'] = 'https://' . $_SERVER['HTTP_HOST'];
+
+// Lanjutkan memuat index.php dari public/
 require __DIR__ . "/../public/index.php";
