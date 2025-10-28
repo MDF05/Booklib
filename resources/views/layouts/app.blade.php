d63e4bf52434e5f1e0f578eb31039030abcd41cb
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-10-28 17:24:19 +0700

    fix: tailwind css config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!--  // ! Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
+    <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 
